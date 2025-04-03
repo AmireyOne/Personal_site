7fb2a548b54453ddf1232569b33b59fb2367800d
amireyone <amireyone@gmail.com>
2025-04-03 23:57:08 +0330

    designed setting modal

--- js/js_personalSite.js
+++ js/js_personalSite.js
@@ -1,7 +1,34 @@
 const headerNav = document.querySelector('.header-nav')
 const bergerBotton = document.querySelector('.burger-btn-wrapper')
+const dayMoodBotton = document.querySelector('.day-mood')
+const nigthMoodBotton = document.querySelector('.night-mood')
+
+// refresh site
+
+window.addEventListener('DOMContentLoaded' , ()=>{
+    let siteMood = localStorage.getItem('siteMood')
+
+    if (siteMood === "day"){
+        document.body.classList.remove('dark-them')
+    }else{
+        document.body.classList.add('dark-them')
+    }
+});
+// close and open menu mobile
 
 bergerBotton.addEventListener('click' , ()=>{
     headerNav.classList.toggle('open-list')
     bergerBotton.classList.toggle('change-icon')
 })
+
+// nigth and day mood 
+nigthMoodBotton.addEventListener('click' , ()=>{
+    document.body.classList.add('dark-them')
+    localStorage.setItem('siteMood' , 'nigtht')
+})
+
+dayMoodBotton.addEventListener('click' , ()=>{
+    document.body.classList.remove('dark-them')
+    localStorage.setItem('siteMood' , 'day')
+})
+

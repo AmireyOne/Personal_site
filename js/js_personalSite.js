e1b108b1f394a6fd296f70811e9b7526823e3599
amireyone <amireyone@gmail.com>
2025-04-05 20:15:30 +0330

    100% section home and about me

--- js/js_personalSite.js
+++ js/js_personalSite.js
@@ -1,34 +1,52 @@
-const headerNav = document.querySelector('.header-nav')
-const bergerBotton = document.querySelector('.burger-btn-wrapper')
-const dayMoodBotton = document.querySelector('.day-mood')
-const nigthMoodBotton = document.querySelector('.night-mood')
+const headerNav = document.querySelector(".header-nav");
+const bergerBotton = document.querySelector(".burger-btn-wrapper");
+const dayMoodBotton = document.querySelector(".day-mood");
+const nigthMoodBotton = document.querySelector(".night-mood");
+const nav_links = document.querySelectorAll(".nav-link");
+const header = document.querySelector(".header");
+const home_section = document.querySelector('#home')
 
 // refresh site
 
-window.addEventListener('DOMContentLoaded' , ()=>{
-    let siteMood = localStorage.getItem('siteMood')
+window.addEventListener("DOMContentLoaded", () => {
 
-    if (siteMood === "day"){
-        document.body.classList.remove('dark-them')
-    }else{
-        document.body.classList.add('dark-them')
-    }
+    
+
+
+  let siteMood = localStorage.getItem("siteMood");
+
+  if (siteMood === "day") {
+    document.body.classList.remove("dark-them");
+  } else {
+    document.body.classList.add("dark-them");
+  }
 });
 // close and open menu mobile
 
-bergerBotton.addEventListener('click' , ()=>{
-    headerNav.classList.toggle('open-list')
-    bergerBotton.classList.toggle('change-icon')
-})
+bergerBotton.addEventListener("click", () => {
+  headerNav.classList.toggle("open-list");
+  bergerBotton.classList.toggle("change-icon");
+});
 
-// nigth and day mood 
-nigthMoodBotton.addEventListener('click' , ()=>{
-    document.body.classList.add('dark-them')
-    localStorage.setItem('siteMood' , 'nigtht')
-})
+// nigth and day mood
+nigthMoodBotton.addEventListener("click", () => {
+  document.body.classList.add("dark-them");
+  localStorage.setItem("siteMood", "nigtht");
+});
 
-dayMoodBotton.addEventListener('click' , ()=>{
-    document.body.classList.remove('dark-them')
-    localStorage.setItem('siteMood' , 'day')
-})
+dayMoodBotton.addEventListener("click", () => {
+  document.body.classList.remove("dark-them");
+  localStorage.setItem("siteMood", "day");
+});
 
+nav_links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetSection = document.querySelector(targetId);
+
+    window.scrollTo({
+      top: targetSection.offsetTop  - header.getBoundingClientRect().height ,
+    });
+  });
+});
